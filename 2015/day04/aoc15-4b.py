85bda240b0d442bee7f45d44deff886a0695e6c5
Azcobu <81782124+Azcobu@users.noreply.github.com>
2022-01-01 19:05:41 +1030

    .

--- 2015/day04/aoc15-4b.py
+++ 2015/day04/aoc15-4b.py
@@ -1,15 +1,14 @@
 import hashlib
+import itertools
 
 def find_target(prefix):
-    for x in range(100000000):
+    for x in itertools.count():
         cand = prefix + str(x)
         md5str = hashlib.md5(cand.encode('utf-8')).hexdigest()
         if md5str.startswith('000000'):
             return x
 
 def main():
-    #assert find_target('abcdef') == 609043
-    #assert find_target('pqrstuv') == 1048970
     print(find_target('ckczppom'))
 
 if __name__ == '__main__':
