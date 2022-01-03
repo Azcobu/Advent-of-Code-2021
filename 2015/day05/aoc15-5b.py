5a4a2cc579d744752f96e3937ca446a78d541175
Azcobu <81782124+Azcobu@users.noreply.github.com>
2022-01-03 16:07:46 +1030

    Update aoc15-5b.py

--- 2015/day05/aoc15-5b.py
+++ 2015/day05/aoc15-5b.py
@@ -1,35 +1,20 @@
-# AoC 2015 - Day 5
+# AoC 2015 - Day 5b
+import re
 
 def load_data():
     with open('input.txt', 'r') as infile:
         return [x.strip() for x in infile.readlines()]
 
-def check_vowels(instr):
-    return sum([1 for x in instr if x in ['a', 'e', 'i', 'o', 'u']]) >= 3
-
-def check_twicerow(instr):
-    for x in set(instr):
-        if x + x in instr:
-            return True
-    return False
-
-def check_forbidden(instr):
-    for x in ['ab', 'cd', 'pq', 'xy']:
-        if x in instr:
-            return True
-    return False
-
-def checker(instr):
-    return not check_forbidden(instr) and check_vowels(instr) and check_twicerow(instr)
+def nice2(instr):
+    return re.search(r"(..).*\1", instr) != None and re.search(r"(.).\1", instr) != None
 
 def main():
-    print(checker('ugknbfddgicrmopn'))
-    print(checker('aaa'))
-    print(checker('jchzalrnumimnmhp'))
-    print(checker('haegwjzuvuyypxyu'))
-    print(checker('dvszwmarrgswjxmb'))
+    assert nice2('qjhvhtzxzqqjkmpb') == True
+    assert nice2('xxyxx') == True
+    assert nice2('uurcxstgmygtbstg') == False
+    assert nice2('ieodomkazucvgmuy') == False
     indata = load_data()
-    print(sum([1 for x in indata if checker(x)]))
+    print(sum([1 for x in indata if nice2(x)]))
 
 if __name__ == '__main__':
     main()
