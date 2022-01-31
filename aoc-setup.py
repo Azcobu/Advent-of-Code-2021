f0b464979452d0d2ff8c0fda79e4848e8b756446
Azcobu <81782124+Azcobu@users.noreply.github.com>
2022-01-31 14:01:42 +1030

    Mass download all inputs

---
+++ aoc-setup.py
@@ -0,0 +1,32 @@
+import os
+import requests
+import time
+
+# https://adventofcode.com/2019/day/17/input
+cookie = '53616c7465645f5f4fde66eab475152c843a9204747747071f0f05b4682ecfd0f86f34ffcbd37eb8150dea629926f217'
+cookie_dict = {'session':cookie}
+
+
+def main():
+    basepath = 'D:\\Python\\Code\\Advent-of-Code-2021\\'
+    years = [str(y) for y in range(2015, 2021)]
+
+    for y in years:
+        currpath = os.path.join(basepath, y)
+        for d in range(1, 26):
+            daynum = str(d).zfill(2)
+            dirname = os.path.join(currpath, f'day{daynum}')
+            # os.mkdir(dirname)
+            fname = os.path.join(dirname, 'input.txt')
+            url = f'https://adventofcode.com/{y}/day/{d}/input'
+            try:
+                r = requests.get(url, allow_redirects=True, cookies=cookie_dict)
+                open(fname, 'wb').write(r.content)
+            except Exception as err:
+                print(err)
+            else:
+                print(f'Saved {y} Day {d} input.')
+                time.sleep(1)
+
+if __name__ == '__main__':
+    main()
