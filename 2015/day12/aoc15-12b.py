ecc7fd5adaa4435609e4adfd9d076306971598eb
Azcobu <81782124+Azcobu@users.noreply.github.com>
2022-02-03 07:41:39 +1030

    Update aoc15-12b.py

--- 2015/day12/aoc15-12b.py
+++ 2015/day12/aoc15-12b.py
@@ -1,17 +1,20 @@
 # AoC 15 Day 12a
 import re
+import json
 
 # \{*:"red"*  \}
 
 def load_data():
     with open('input.txt', 'r') as infile:
-        return infile.readlines()
+        return infile.read()
+
+def hook(obj):
+  return {} if "red" in obj.values() else obj
 
 def main():
     data = load_data()
-    for x in data:
-        if re.findall('\{.*:"red".*\}', x):
-            print(x)
+    scan = str(json.loads(data, object_hook=hook))
+    print(sum(map(int, re.findall("-?[0-9]+", scan))))
 
 if __name__ == '__main__':
     main()
