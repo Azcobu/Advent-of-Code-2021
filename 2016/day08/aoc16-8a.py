4c3d1f0f2ce6243a8e48578fa982803ce1478d42
Azcobu <81782124+Azcobu@users.noreply.github.com>
2022-02-13 11:35:08 +1030

    Update aoc16-8a.py

--- 2016/day08/aoc16-8a.py
+++ 2016/day08/aoc16-8a.py
@@ -15,14 +15,33 @@ def load_data():
         instrs.append((i_type, int(m.group(1)), int(m.group(2))))
     return instrs
 
-def rotate(mode, amount):
-    pass
+def rotate(grid, instr):
+    gridsize = 50, 6
+    mode, pos, offset = instr
+    rowcol = 1 if mode == 'x' else 0
+    for node in grid:
+        if node[rowcol] == pos:
+            node[rowcol] += offset
+            if node[rowcol] > gridsize[rowcol]:
+                node[rowcol] = node[rowcol] % gridsize[rowcol]
+    return grid
 
 def parse_instrs(instrs):
-    pass
+    grid = []
+    for i in instrs:
+        if i[0] == 'r':
+            for y in range(i[2]):
+                for x in range(i[1]):
+                    grid.append((x, y))
+        else:
+            grid = rotate(grid, i)
+    return grid
+
 
 def main():
     print(load_data())
+    parse_instrs([('r', 3, 2), ('x', 1, 1)])
+
 
 if __name__ == '__main__':
     main()
