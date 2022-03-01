5645067a9e1249fcd1e270772918605a1c8a6edb
Azcobu <81782124+Azcobu@users.noreply.github.com>
2022-03-02 01:01:38 +1030

    .

---
+++ 2016/day20/aoc16-20b.py
@@ -0,0 +1,22 @@
+def load_data():
+    rules = []
+    with open('input.txt', 'r') as infile:
+        for line in infile.readlines():
+            start, stop = line.strip().split('-')
+            rules.append((int(start), int(stop)))
+    return sorted(rules)
+
+def find_lowest(n, rules):
+    x = 1
+    while True:
+        for r in rules:
+            if r[0] <= x <= r[1]:
+                x = r[1] + 1
+        else:
+            return x
+
+def main():
+    print(find_lowest(4294967295, load_data()))
+
+if __name__ == '__main__':
+    main()
