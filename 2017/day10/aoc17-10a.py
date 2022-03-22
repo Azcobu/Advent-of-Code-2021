e9d2f5bf5cd61a3c8c2d36c6252f0b0851261ea1
Azcobu <81782124+Azcobu@users.noreply.github.com>
2022-03-22 17:41:13 +1030

    Create aoc17-10a.py

---
+++ 2017/day10/aoc17-10a.py
@@ -0,0 +1,22 @@
+# AoC 2017 - Day 10a
+from collections import deque
+
+def load_data():
+    with open('input.txt', 'r') as infile:
+        return [int(x) for x in infile.read().split(',')]
+
+def knothash(listsize, lens):
+    k = deque(range(listsize), listsize)
+    currpos = 0
+    for strlen in lens:
+        k[currpos:currpos+strlen] = k[currpos:currpos+strlen][::-1]
+        print(k)
+
+
+
+def main():
+    print(load_data())
+    print(knothash(5, [3, 4, 1, 5]))
+
+if __name__ == '__main__':
+    main()
