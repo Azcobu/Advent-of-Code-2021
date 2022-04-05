c688eba693b4bb2b3da3153aa490da7673baa402
Azcobu <81782124+Azcobu@users.noreply.github.com>
2022-04-05 15:04:10 +0930

    Update aoc17-10a.py

--- 2017/day10/aoc17-10a.py
+++ 2017/day10/aoc17-10a.py
@@ -1,20 +1,36 @@
 # AoC 2017 - Day 10a
-from collections import deque
 
 def load_data():
     with open('input.txt', 'r') as infile:
         return [int(x) for x in infile.read().split(',')]
 
+def get_eles(startpos, n, inlist):
+    out = []
+    for num in range(n):
+        takepos = (startpos + num) % len(inlist)
+        out.append(inlist[takepos])
+    return out
+
+def rebuild(startpos, revlist, oldlist):
+    for pos, x in enumerate(revlist):
+        addpos = (startpos + pos) % len(oldlist)
+        oldlist[addpos] = x
+    return oldlist
+
 def knothash(listsize, lens):
-    k = deque(range(listsize), listsize)
-    currpos = 0
-    for strlen in lens:
-        k[currpos:currpos+strlen] = k[currpos:currpos+strlen][::-1]
-        print(k)
+    nums = list(range(listsize))
+    currpos, skip = 0, 0
+    for l in lens:
+        revlist = get_eles(currpos, l, nums)[::-1]
+        nums = rebuild(currpos, revlist, nums)
+        currpos = (currpos + l + skip) % len(nums)
+        skip += 1
+    return nums[0] * nums[1]
 
 def main():
-    print(load_data())
+    d = load_data()
     print(knothash(5, [3, 4, 1, 5]))
+    print(knothash(256, d))
 
 if __name__ == '__main__':
     main()
