d323933afaa7a400a1ebfc9cd248869b17ffab76
Azcobu <81782124+Azcobu@users.noreply.github.com>
2022-04-19 12:45:58 +0930

    Update aoc17-15b.py

--- 2017/day15/aoc17-15b.py
+++ 2017/day15/aoc17-15b.py
@@ -1,11 +1,23 @@
 # AoC 2017 - Day 15b
 
+def gen_1(g1):
+    while True:
+        g1 = (g1 * 16807) % 2147483647
+        if g1 % 4 == 0:
+            yield bin(g1)[-16:]
+
+def gen_2(g2):
+    while True:
+        g2 = (g2 * 48271) % 2147483647
+        if g2 % 8 == 0:
+            yield bin(g2)[-16:]
+
 def gen_comp(gen1, gen2):
     score = 0
-    for x in range(40000000):
-        gen1 = gen1 * 16807 % 2147483647
-        gen2 = gen2 * 48271 % 2147483647
-        if bin(gen1)[-16:] == bin(gen2)[-16:]:
+    g1 = gen_1(gen1)
+    g2 = gen_2(gen2)
+    for x in range(5000000):
+        if next(g1) == next(g2):
             score += 1
     return score
 
