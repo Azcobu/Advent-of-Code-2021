f91bff80e1859430a9b3aaa60d0951e658b7dcb8
Azcobu <81782124+Azcobu@users.noreply.github.com>
2022-05-25 08:11:52 +0930

    .

--- 2017/day22/aoc17-22b.py
+++ 2017/day22/aoc17-22b.py
@@ -1,32 +1,33 @@
 #AoC 2017 - Day 22a
 
 def load_data():
-    grid = set()
+    grid = {}
     with open('input.txt', 'r') as infile:
         for rownum, row in enumerate(infile.read().splitlines()):
             for colnum, char in enumerate(row):
                 if char == '#':
-                    grid.add((colnum, rownum))
+                    grid[(colnum, rownum)] = 'I'
     return grid
-
-def move(x, y, curr_dir):
-    moves = [(0, -1), (1, 0), (0, 1), (-1, 0)]
-    return (x + moves[curr_dir][0], y + moves[curr_dir][1])
 
 def count_infections(grid):
     curr_dir, infs = 0, 0
     moves = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    turns = {'C': -1, 'W': 0, 'I': 1, 'F': 2}
+    changes = list(turns.keys())
+
     x = round(max([k[0] for k in grid])/2)
     y = round(max([k[1] for k in grid])/2)
 
-    for burst in range(10000):
-        turn_dir = 1 if (x, y) in grid else -1
+    for burst in range(10000000):
+
+        curr_node = grid[(x, y)] if (x, y) in grid else 'C'
+        turn_dir = turns[curr_node]
         curr_dir = (curr_dir + turn_dir) % 4
 
-        if (x, y) in grid:
-            grid.remove((x, y))
-        else:
-            grid.add((x, y))
+        node_type_idx = (changes.index(curr_node) + 1) % 4
+        grid[(x, y)] = changes[node_type_idx]
+
+        if node_type_idx == 2:
             infs += 1
 
         x, y = (x + moves[curr_dir][0], y + moves[curr_dir][1])
