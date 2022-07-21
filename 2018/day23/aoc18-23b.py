50cf033fd512c7457f3ca685772c46466429fa83
Azcobu <81782124+Azcobu@users.noreply.github.com>
2022-07-21 17:18:17 +0930

    .

--- 2018/day23/aoc18-23b.py
+++ 2018/day23/aoc18-23b.py
@@ -20,29 +20,66 @@ def gen_init_pop(probes, popsize):
         pop[(random.randint(min_x, max_x), random.randint(min_y, max_y), random.randint(min_z, max_z))] = 0
     return pop
 
-def calc_fitness(probes, pop):
+def find_fittest(probes, pop, nextgenpcent):
     for pop_coords, pop_score in pop.items():
-        for probe_num, probe_coords in probes.items():
-            dist = sum([abs(pop_coords[x] - probe_coords[x]) for x in range(3)])
-            if dist <= probe_coords[3]:
-                pop_score += 1
-    print(sorted(pop.items(), key=lambda x:x[1], reverse=True)[:10])
-
-def find_in_rad(probes):
-    count = 0
-    bigprobe, bigcoords = sorted(probes.items(), key=lambda x:x[1][3])[-1]
+        if pop_score == 0:
+            for probe_num, probe_coords in probes.items():
+                dist = sum([abs(pop_coords[x] - probe_coords[x]) for x in range(3)])
+                if dist <= probe_coords[3]:
+                    pop[pop_coords] += 1
+    x = sorted(pop.items(), key=lambda x:(x[1], -sum(x[0])), reverse=True)[:int(len(pop) * nextgenpcent)]
+    print(f'Current high score is {x[0]}')
+    return {k:v for k, v in x}
     
-    for k, v in probes.items():
-        dist = sum([abs(bigcoords[x] - v[x]) for x in range(3)])
-        if dist <= bigcoords[3]:
-            count += 1
-    return count
+def find_optimal(probes, popsize):
+    gencount = 0
+    bestset = []
+
+    pop = gen_init_pop(probes, popsize)
+    for x in range(100):
+        gencount += 1
+        pop = find_fittest(probes, pop, 0.1)
+        print(f'(Gen {gencount}')
+        pop = breed_sets(pop, popsize, gencount)
+
+def breed_sets(pop, popsize, gencount):
+    probelist = list(pop.keys())
+    while len(pop) < popsize:
+        if random.randint(1, 2) == 1:
+            newprobe = crossbreed(random.choice(probelist), random.choice(probelist))
+        else:
+            newprobe = parthenogen(random.choice(probelist), gencount)
+        pop[newprobe] = 0
+    return pop
+
+def crossbreed(p1, p2):
+    return tuple([(p1[x] + p2[x])//2 for x in range(3)])
+
+def parthenogen(p1, gencount):
+    if gencount <10:
+        step = 10000000
+    elif gencount < 20:
+        step = 1000000
+    elif gencount < 30:
+        step = 100000
+    elif gencount < 40:
+        step = 10000
+    elif gencount < 50:
+        step = 1000
+    elif gencount < 60:
+        step = 100
+    else:
+        step = 10
+
+
+    new = [0, 0, 0]
+    for x in range(3):
+        new[x] = p1[x] + random.randint(-step, step)
+    return tuple(new)
 
 def main():
-    data = load_data()
-    print(find_in_rad(data))
-    pop = gen_init_pop(data, 1000)
-    calc_fitness(data, pop)
+    probes = load_data()
+    find_optimal(probes, 2000)
 
 if __name__ == '__main__':
     main()
