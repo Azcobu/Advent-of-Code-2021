b7e209822995045567c8a4fe144bcac829df5e6b
Azcobu <81782124+Azcobu@users.noreply.github.com>
2022-07-28 14:26:28 +0930

    Update aoc2015-22a.py

--- 2015/day22/aoc2015-22a.py
+++ 2015/day22/aoc2015-22a.py
@@ -1,17 +1,5 @@
 # AoC 2015 - Day 22a
-import itertools, copy
-
-class Effect:
-    def __init__(self, name, base_cooldown, dmg=0, armour=0, mana=0):
-        self.name = name
-        self.base_cooldown = base_cooldown
-        self.curr_cooldown = 0
-        self.dmg = dmg
-        self.armour = armour
-        self.mana = mana
-
-    def __repr__(self):
-        return f'Effect: {self.name}, {self.cooldown} turns left'
+import itertools, copy, random
 
 class Spell:
     def __init__(self, name, cost, dmg=0, heal=0, effect=None):
@@ -23,6 +11,18 @@ class Spell:
 
     def __repr__(self):
         return f'Spell: {self.name}'
+
+class Effect:
+    def __init__(self, name, base_cd, dmg=0, armour=0, mana=0):
+        self.name = name
+        self.base_cd = base_cd
+        self.curr_cd = 0
+        self.dmg = dmg
+        self.armour = armour
+        self.mana = mana
+
+    def __repr__(self):
+        return f'Effect: {self.name}, {self.curr_cd} of {self.base_cd} turns left'
 
 def init_spells():
     eff_shield = Effect('eff_shield', 6, 0, 7)
@@ -48,68 +48,63 @@ class Entity:
     def __repr__(self):
         return f'Entity: {self.name}, HP: {self.curr_hp}, Arm: {self.armour}'
 
-def find_optimal(player, boss, spells, castorder):
-    min_mana = 99999
+def decrement_effect_cds(player, boss, spells):
+    for s in spells:
+        if s.effect:
+            if 0 < s.effect.curr_cd <= s.effect.base_cd:
+                boss.curr_hp -= s.effect.dmg
+                player.mana += s.effect.mana
 
-    for castorder in itertools.product([4, 3, 2, 1, 0], repeat=10):
-        c_player, c_boss = copy.deepcopy(player), copy.deepcopy(boss)
-        used_mana = sim_combat(c_player, c_boss, spells, castorder, min_mana)
-        if used_mana:
-            print(castorder)
-            print(f'{used_mana}')
-            min_mana = min(min_mana, used_mana)
+            if s.effect.curr_cd > 0:
+                s.effect.curr_cd -= 1
 
-    return min_mana
+def sim_fights(i_player, i_boss, i_spells):
+    mana_results = {}
+    test_spells = [3, 0]
 
-def poss_casts(spells):
-    return [s for s in spells if not s.effect or s.effect.curr_cooldown == 0]
+    for fight in range(100):
+        mana_used = 0
+        spells_used = []
+        player, boss, spells = copy.deepcopy(i_player), copy.deepcopy(i_boss), copy.deepcopy(i_spells)
 
-def sim_combat(player, boss, spells, castorder, min_mana=99999):
-    mana_used = 0
+        while player.curr_hp > 0 and boss.curr_hp > 0:
 
-    for c in castorder:
-        currspell = spells[c]
+            if mana_results:
+                if mana_used > min(mana_results.values()):
+                    break
 
-        if currspell.effect and currspell.effect.curr_cooldown >= 0:
-            return None
+            spell_choices = [s for s in spells if not s.effect or s.effect.curr_cd == 0]
+            currspell = random.choice(spell_choices)
+            spells_used.append(currspell.name)
+            mana_used += currspell.cost
+            if currspell.effect:
+                currspell.effect.curr_cd = currspell.effect.base_cd
+            boss.curr_hp -= currspell.dmg
+            player.curr_hp = min(player.curr_hp + currspell.heal, player.max_hp)
 
-        mana_used += currspell.cost
-        if currspell.effect:
-            currspell.curr_cooldown = currspell.effect.base_cooldown
-        boss.curr_hp -= currspell.dmg
-        player.curr_hp = min(player.curr_hp + currspell.heal, player.max_hp)
+            # handle effects
+            decrement_effect_cds(player, boss, spells)
 
-        # handle effects
-        for s in spells:
-            if s.effect:
-                if 0 < s.effect.curr_cooldown < s.effect.max_cooldown:
-                    boss.hp -= s.effect.dmg
-                    player.mana += s.effect.mana
+            if spells[2].effect.curr_cd > 0: # shield
+                player.armour = spells[2].effect.armour
+            else:
+                player.armour = 0
 
-                if s.effect.curr_cooldown > 0:
-                    s.effect.cooldown -= 1
+            #boss turn
+            decrement_effect_cds(player, boss, spells)
+            player.curr_hp -= max(boss.dmg - player.armour, 1)
 
-        if s.name == 'Shield' and s.effect.curr_cooldown > 0:
-            player.armour = s.effect.armour
-        else:
-            player.armour = 0
-
-        if mana_used > min_mana:
-            return
-
-        #boss turn
-        player.curr_hp -= max(boss.dmg - player.armour, 1)
-        if player.curr_hp <= 0:
-            return None
-
-    return mana_used
-
+        if player.curr_hp > 0:
+            mana_results[tuple(spells_used)] = mana_used
+    
+    #print(mana_results)
+    return sorted(mana_results.items(), key=lambda x:x[1])[0]
+ 
 def main():
-    boss = Entity('boss', 58, 58, 0, 9)
-    player = Entity('player', 50, 50, 0, 0, 500)
+    test_boss = Entity('boss', 13, 13, 0, 8)
+    test_player = Entity('player', 10, 10, 0, 0, 250)
     spells = init_spells()
-    castorder = [0, 1, 0]
-    print(find_optimal(player, boss, spells, castorder))
+    print(sim_fights(test_player, test_boss, spells))
 
 if __name__ == '__main__':
     main()
