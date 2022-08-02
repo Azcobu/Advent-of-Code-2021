ca2c9f48e9ec6eff20509a269aeb90fadb1b32c3
Azcobu <81782124+Azcobu@users.noreply.github.com>
2022-08-02 16:43:51 +0930

    .

--- 2018/day24/aoc18-24a.py
+++ 2018/day24/aoc18-24a.py
@@ -2,10 +2,12 @@
 
 from dataclasses import dataclass
 from parse import parse
+import logging
 
 @dataclass
 class Group:
     side: str
+    name: str
     num: int
     hp: int
     dmg: int
@@ -13,41 +15,81 @@ class Group:
     init: int
     weak: list
     immune: list
-    effectpow = 0
-    targeting = None
-    targeted = False
+    power = 0
+    target = None
+    is_targeted = False
 
+def handle_weakimmune(instr):
+    weak, immune = [], []
+    instr = instr[1:-1].split(';') if ';' in instr else [instr[1:-1]]
+    for part in instr:
+        if 'weak to' in part:
+            weak = [x.strip() for x in part[8:].split(', ')]
+        if 'immune to' in part:
+            immune = [x.strip() for x in part[10:].split(', ')]
+    return weak, immune
+        
 def load_data():
     groups = []
-    side = 'immune'
+    side = 'Immune'
+    counter = 1
 
-    with open('input.txt', 'r') as infile:
+    with open('example.txt', 'r') as infile:
         for line in [x.strip() for x in infile.readlines()]:
-            if 'Infection' in line:
-                side = 'infect'
+            if not line or 'Immune System' in line:
+                continue
+            elif 'Infection' in line:
+                side = 'Infection'
+                counter = 1
+            else:
+                parsestr = '{} units each with {} hit points {} with an attack that does {} {} damage at initiative {}'
+                num, hp, weakimm, dmg, dmgtype, init = parse(parsestr, line)
+                weak, immune = handle_weakimmune(weakimm)
+                name = f'{side} group {counter}'
+                counter += 1
+                groups.append(Group(side, name, int(num), int(hp), int(dmg), dmgtype, int(init), weak, immune))
+    return groups
 
-            parsestr = '{} units each with {} hit points {} with an attack that does {} {} damage at initiative {}'
-            num, hp, weak, dmg, dmgtype, init = parse(parsestr, line)
-            units.append(Group(side, num, hp, dmg, dmgtype, init, weak, immune))
-    return units
+def show_status(groups):
+    for side in ['Immune', 'Infection']:
+        logging.debug(f'{side}:')
+        for g in groups:
+            if side in g.name:
+                logging.debug(f'{g.name} contains {g.num} units.')
 
 def sim_battle(groups):
-    #set effect pow and reset targeting
-    for g in groups:
-        g.effectpow = g.num * g.dmg
-        g.targeting = None
-        g.targeted = False
 
-    for curr in sorted(groups, key=lambda x:(x.effectpow, x.init), reverse=True):
-        pass
+    while True:
+        for g in groups:
+            g.power = g.num * g.dmg
+            g.target = None
+            g.is_targeted = False
+
+        show_status(groups)
+
+        for curr in sorted(groups, key=lambda x:(-x.power, -x.init)):
+            poss = [g for g in groups if g.side != curr.side if g.is_targeted == False and curr.dmgtype not in g.immune]
+            poss.sort(key=lambda x:(curr.dmgtype not in x.weak, -x.power, -x.init))
+            if poss:
+                curr.target = poss[0]
+                poss[0].is_targeted = True
+                logging.debug(f'{curr.name} targets {curr.target.name}')
+
+        for curr in sorted([x for x in groups if x.target], key=lambda x:-x.init):
+            dmgmult = 2 if curr.dmgtype in curr.target.weak else 1
+            dmgdone = curr.power * dmgmult
+            loss = dmgdone // curr.target.hp
+            curr.target.num -= loss
+            logging.debug(f'{curr.name} attacks {curr.target.name} for {dmgdone}, destroying {loss} units.')
+
+        groups = [x for x in groups if x.num > 0]
+        if len(set([x.side for x in groups])) == 1:
+            return sum([x.num for x in groups])
 
 def main():
-
-    test = [Group('immune', 17, 5390, 4507, 'fire', 2, ['radiation', 'bludgeoning'], []), 
-            Group('immune', 989, 1274, 25, 'slashing', 3, ['bludgeoning', 'slashing'], ['fire']),
-            Group('infect', 801, 4706, 116, 'bludgeoning', 1, ['radiation'], []),
-            Group('infect', 4485, 2961, 12, 'slashing', 4, ['fire', 'cold'], ['radiation'])]
-    sim_battle(test)
+    logging.basicConfig(level=logging.DEBUG, format='%(message)s')
+    groups = load_data()
+    print(sim_battle(groups))
 
 if __name__ == '__main__':
     main()
