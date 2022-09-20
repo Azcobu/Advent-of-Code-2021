cd3126e62c994127ca8a64888f3c621763b28d3f
Azcobu <81782124+Azcobu@users.noreply.github.com>
2022-09-21 00:28:06 +0930

    Create aoc2020-17b.py

---
+++ 2020/day17/aoc2020-17b.py
@@ -0,0 +1,45 @@
+# AoC 2020 Day 17a
+from itertools import product
+
+def load_data():
+    grid = set()
+    with open('input.txt', 'r') as infile:
+        d = infile.read().splitlines()
+    for rownum, line in enumerate(d):
+        for colnum, char in enumerate(line):
+            if char == '#':
+                grid.add((colnum, rownum, 0, 0))
+    return grid
+
+def count_neighbours(grid, cube):
+    count = 0
+    for offset in product([-1, 0, 1], repeat=4):
+        if offset != (0, 0, 0, 0):
+            if (cube[0] + offset[0], cube[1] + offset[1], cube[2] + offset[2], cube[3] + offset[3]) in grid:
+                count += 1
+    return count
+
+def count_active(grid, cycles):
+    gridstart = 0
+    gridsize = max(x[0] for x in grid)
+
+    for tick in range(cycles):
+        newgrid = set()
+
+        for cube in product(range(gridstart - 1, gridsize + 2), repeat=4):
+            if cube in grid and count_neighbours(grid, cube) in [2, 3]:
+                newgrid.add(cube)
+            if cube not in grid and count_neighbours(grid, cube) == 3:
+                newgrid.add(cube)
+        
+        grid = newgrid
+        gridstart -= 1
+        gridsize += 1
+    
+    return len(grid)
+
+def main():
+    print(count_active(load_data(), 6))
+
+if __name__ == '__main__':
+    main()
