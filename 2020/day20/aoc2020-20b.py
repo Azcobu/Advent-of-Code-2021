22136c42945eaa5a431e5110dcf6ce3ec16dbd10
Azcobu <81782124+Azcobu@users.noreply.github.com>
2022-09-27 16:25:38 +0930

    Create aoc2020-20b.py

---
+++ 2020/day20/aoc2020-20b.py
@@ -0,0 +1,56 @@
+# AoC 2020 Day 20b
+
+def load_data():
+    tiles = {}
+    with open('example.txt', 'r') as infile:
+        d = infile.read()
+    for t in d.split('\n\n'):
+        tile = t.split('\n')
+        tilename = tile[0][5:-1]
+        tilesize = len(tile[1])
+        top = tile[1]
+        bottom = tile[tilesize]
+        left = ''.join([x[0] for x in tile[1:]])
+        right = ''.join([x[tilesize-1] for x in tile[1:]])
+        tiles[tilename] = [top, right, bottom, left]
+    return tiles
+
+def find_match(tiles, curr_tile, side):
+    for tile, sides in tiles.items():
+        if tile != curr_tile:
+            for s in sides:
+                if s == side or s[::-1] == side:
+                    return True
+    return False 
+
+def strip_borders(tile):
+    newtile = []
+    for rownum, row in enumerate(tile):
+        if rownum not in [0, len(tile)]:
+            newtile.append([tile[1:-1]])
+    return newtile
+
+def rotate_tile(tile):
+    return [''.join(x) for x in list(zip(*tile[::-1]))]
+
+def find_corners(tiles):
+    corner_ids = 1
+    sidematches = {x: 0 for x in tiles.keys()}
+
+    for curr_tile, sides in tiles.items():
+        for side in sides:
+            if find_match(tiles, curr_tile, side):
+                sidematches[curr_tile] += 1
+    
+    for k, v in sidematches.items():
+        if v == 2:
+            corner_ids *= int(k)
+    return corner_ids
+
+def main():
+    test = ['#..', '...', '...']
+    print(rotate_tile(test))
+    #print(find_corners(load_data()))
+
+if __name__ == '__main__':
+    main()
