6ae9a479481fe11f3b978379fb5792820e271ef8
Azcobu <81782124+Azcobu@users.noreply.github.com>
2023-12-22 15:23:39 +1030

    .

--- 2023/day10/aoc23-10b.py
+++ 2023/day10/aoc23-10b.py
@@ -1,4 +1,4 @@
-# AoC 2023 Day 10a
+# AoC 2023 Day 10b
 
 def load_data():
     grid = {}
@@ -8,15 +8,31 @@ def load_data():
                 grid[(colnum, rownum)] = char
     return grid
 
+def count_enclosed(grid, loop):
+    enclosed = 0
+    max_x = max(x[0] for x in grid.keys())
+    max_y = max(x[1] for x in grid.keys())
+
+    for y in range(max_y):
+        for x in range(max_x):
+            if (x, y) not in loop:
+                maxsteps = min(x, y)
+                linecoords = [(x - k, y - k) for k in range(maxsteps + 1)]
+                intersects = sum([1 for x in linecoords if grid[x] in '|-FJ' and x in loop])
+                if intersects % 2 == 1:
+                    enclosed += 1
+    print(enclosed)
+
 def main():
     dirs = {'N': (0, -1) , 'E': (1, 0), 'S': (0, 1), 'W': (-1, 0)}
+    startdirs = {'N': '|F7', 'E': '-J7', 'S': '|JL', 'W': '-LF'}
     transitions = {('N', '7'): 'W', ('N', 'F'): 'E', ('S', 'L'): 'E', ('S', 'J'): 'W',\
                    ('E', 'J'): 'N', ('E', '7'): 'S', ('W', 'L'): 'N', ('W', 'F'): 'S'}
-    startdirs = {'N': '|F7', 'E': '-J7', 'S': '|JL', 'W': '-LF'}
     grid = load_data()
     start = [k for k, v in grid.items() if v == 'S'][0]
     currpos = start
-    steps = 0
+    currdir = 'N'
+    loop = set()
 
     for k, v in dirs.items():
         cand = (start[0] + v[0], start[1] + v[1])
@@ -28,12 +44,15 @@ def main():
         nextpos = dirs[currdir]
         currpos = (currpos[0] + nextpos[0], currpos[1] + nextpos[1])
         currchar = grid[currpos]
-        steps += 1
+        loop.add(currpos)
         if currchar == 'S':
             break
         if currchar not in '|-':
             currdir = transitions[(currdir, currchar)]
-    print(steps//2)
+
+    # set S pipe - ex2 = F, ex3 = F, ex4 = 7, input = J
+    grid[start] = 'J'
+    count_enclosed(grid, loop)
 
 if __name__ == '__main__':
     main()
