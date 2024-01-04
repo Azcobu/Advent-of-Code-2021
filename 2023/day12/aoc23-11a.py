97d710526d75cfd84f83c323e9aefb0556ed4ab1
Azcobu <81782124+Azcobu@users.noreply.github.com>
2024-01-04 15:37:33 +1030

    .

--- 2023/day12/aoc23-11a.py
+++ 2023/day12/aoc23-11a.py
@@ -2,34 +2,31 @@
 
 def load_data():
     data = []
-    with open('example.txt', 'r', encoding='utf-8') as infile:
+    with open('input.txt', 'r', encoding='utf-8') as infile:
         for line in infile.readlines():
             a, b = line.split()
             data.append((a, [int(x) for x in b.split(',')]))
     return data
 
 def validate(row, groups):
-    if not row and not groups:
-        return 1
-    else:
-        if row[0] == '.':
-            return validate(row[1:], groups)
-        elif row[0] == '#':
-            if len(row) >= groups[0] and '.' not in row[:groups[0]]:
-                if row[groups[0] + 1] == '.':
-                    return validate(row[groups[0]:], groups[1:])
-                elif row[groups[0] + 1] == '?':
-                    newrow = row[:groups[0]] + '.' + row[groups[0] + 2:]
-                    return validate(newrow, groups[1:])
-                else:
-                    return 0
-            else:
-                return 0
-        elif row[0] == '?':
-            return validate('.' + row[1:], groups) + validate('#' + row[1:], groups)
+    if not row:
+        return len(groups) == 0 
+    if not groups:
+        return '#' not in row
+
+    if row[0] == '.':
+        return validate(row[1:], groups)
+    elif row[0] == '#':
+        if len(row) >= groups[0] and '.' not in row[:groups[0]]:
+            if len(row) == groups[0] or row[groups[0]] != '#':
+                return validate(row[groups[0]+1:], groups[1:])
+            return 0
+        return 0
+    elif row[0] == '?':
+        return validate('.' + row[1:], groups) + validate('#' + row[1:], groups)
 
 def main():
-    print(validate('???.###', [1,1,3]))
+    print(sum(validate(*d) for d in load_data()))
 
 if __name__ == '__main__':
     main()
