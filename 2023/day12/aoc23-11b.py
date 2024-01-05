26066af97e5014d370f29311015ea9e6c2c2ef25
Azcobu <81782124+Azcobu@users.noreply.github.com>
2024-01-05 12:52:21 +1030

    Create aoc23-11b.py

---
+++ 2023/day12/aoc23-11b.py
@@ -0,0 +1,35 @@
+# AoC 23 11b
+from functools import cache
+
+def load_data():
+    data = []
+    with open('input.txt', 'r', encoding='utf-8') as infile:
+        for line in infile.readlines():
+            a, b = line.split()
+            a = '?'.join([a] * 5)
+            data.append((a, tuple([int(x) for x in b.split(',')] * 5)))
+    return tuple(data)
+
+@cache
+def validate(row, groups):
+    if not row:
+        return len(groups) == 0 
+    if not groups:
+        return '#' not in row
+
+    if row[0] == '.':
+        return validate(row[1:], groups)
+    elif row[0] == '#':
+        if len(row) >= groups[0] and '.' not in row[:groups[0]]:
+            if len(row) == groups[0] or row[groups[0]] != '#':
+                return validate(row[groups[0]+1:], groups[1:])
+            return 0
+        return 0
+    elif row[0] == '?':
+        return validate('.' + row[1:], groups) + validate('#' + row[1:], groups)
+
+def main():
+    print(sum(validate(*d) for d in load_data()))
+
+if __name__ == '__main__':
+    main()
