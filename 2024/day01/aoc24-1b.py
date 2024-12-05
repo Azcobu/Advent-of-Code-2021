1ffc9e374a714a2761b07232b3cd490df458d6f6
Azcobu <81782124+Azcobu@users.noreply.github.com>
2024-12-05 23:29:19 +1030

    day1

---
+++ 2024/day01/aoc24-1b.py
@@ -0,0 +1,19 @@
+# AoC 2024 Day 1a
+
+def load_data() -> tuple:
+    l1, l2 = [], []
+
+    with open('input.txt', 'r', encoding='utf-8') as infile:
+        for x in infile.readlines():
+            a, b = x.strip().split()
+            l1.append(int(a))
+            l2.append(int(b))
+    return sorted(l1), sorted(l2)
+
+def main():
+    l1, l2 = load_data()
+    total = sum([x * l2.count(x) for x in l1])
+    print(total)
+
+if __name__ == '__main__':
+    main()
