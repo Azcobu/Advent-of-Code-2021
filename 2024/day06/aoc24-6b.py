96546c3b0462cacd13b2dfe636d8073fc2d55414
Azcobu <81782124+Azcobu@users.noreply.github.com>
2024-12-18 17:14:37 +1030

    Update aoc24-6b.py

--- 2024/day06/aoc24-6b.py
+++ 2024/day06/aoc24-6b.py
@@ -8,11 +8,12 @@ def load_data() -> dict:
                 grid[(colnum, rownum)] = symbol
     return grid
 
-def is_cyclic(grid: dict, start: tuple, facing: str,changed: tuple = (-1, -1)) -> bool:
+def is_cyclic(grid: dict, start: tuple, changed: tuple = (-1, -1)) -> tuple:
     dirs = {'N': (0, -1), 'E': (1, 0), 'S': (0, 1), 'W': (-1, 0)}
+    visited = set()
     pos = start
-    visited = []
-    visited.append((start, facing))
+    facing ='N'
+    visited.add((pos, facing))
 
     newgrid = grid.copy()
     newgrid[changed] = '#'
@@ -20,30 +21,26 @@ def is_cyclic(grid: dict, start: tuple, facing: str,changed: tuple = (-1, -1)) -
     while True:
         next_pos = (pos[0] + dirs[facing][0], pos[1] + dirs[facing][1])
         if next_pos not in newgrid:
-            return visited if changed == (-1, -1) else False
+            return visited if changed == (-1, -1) else None
         if newgrid[next_pos] == '#':
             facing = {'N': 'E', 'E': 'S', 'S': 'W', 'W': 'N'}[facing]
         else:
             pos = next_pos
             if (pos, facing) in visited:
-                print(f'Cycle found for {changed}')
-                return True
+                return changed
             else:
-                visited.append((pos, facing))
+                visited.add((pos, facing))
  
 def main():
-    loops = 0
     grid = load_data()
     start = [k for k, v in grid.items() if v == '^'][0]
-    base_path = [x for x in (is_cyclic(grid, start, 'N'))]
+    base_path = [x[0] for x in (is_cyclic(grid, start))]
+    found = set()
 
-    for pos, start in enumerate(base_path[:-1]):
-        obs = base_path[pos + 1]
-        print(f'{pos} of {len(base_path) - 1}: {start[0]} - {obs[0]}')
-        if is_cyclic(grid, start[0], start[1], obs[0]):
-            loops += 1
-    print(loops)
-
+    for obs in base_path:
+        if is_cyclic(grid, start, obs):
+            found.add(obs)
+    print(len(found))
 
 if __name__ == '__main__':
     main()
